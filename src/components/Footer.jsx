d346cb1cd7a3e3d41b2db61f8638fc1e4f192316
Catalina Ordenes <catalina.ordenes@mail.udp.cl>
2023-05-17 21:15:08 -0400

    Estructura del Navbar y Footer ok, Primeros ensambles de los componentes en App.js ok

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className='footer'>
+            <p>Contacto: modernización@subdere.gov.cl</p>
+        </footer>
+  )
+}
+
+export default Footer
