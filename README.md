1c65aea026b4a8f6570cafb2b3e59f6b306f4c4c
Catalina Ordenes <catalina.ordenes@mail.udp.cl>
2023-05-17 22:36:15 -0400

    Filtro hecho, Css terminado, Readme para fork listo

--- README.md
+++ README.md
@@ -1,70 +1,20 @@
-# Getting Started with Create React App
+# Cómo Instalar este repositorio en tu computador
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+1.-Crea una carpeta en tu computador donde irá el repositorio que vas a forkear
 
-## Available Scripts
+2.-Haz clic en el botón "Fork" en la parte superior derecha de la página del repositorio. Esto creará una copia del repositorio en tu cuenta de GitHub
 
-In the project directory, you can run:
+3.-Después de hacer "fork" del repositorio, ve a tu cuenta de GitHub y selecciona el repositorio que acabas de forkear
 
-### `npm start`
+4.-Haz clic en el botón verde "Code" y copia la URL del repositorio clonando. Debería verse como https://github.com/catalinaordenes/libreria-prueba.git
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+5.-Abre una terminal en tu computadora y navega al directorio donde deseas clonar el repositorio (es la carpeta que creaste en el paso 1)
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+6.-Ejecuta el siguiente comando en la terminal para clonar el repositorio en tu computadora: git clone https://github.com/catalinaordenes/libreria-prueba.git
 
-### `npm test`
+7.-Si todo está bien, con npm start se abriría en tu navegador la página funcionando.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+**En caso de que no te aparezcan los node_modules y el package-lock.json en la consola escribe npm install y vuelve a probar con npm start**
 
-### `npm run build`
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,20 +2,31 @@ import React, { useEffect, useState } from 'react';
 
 
 function Books() {
+    // Aquí definimos los estados iniciales utilizando el hook useState.
     const [books, setBooks] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const booksPerPage = 20;
+    const [filter, setFilter] = useState('');
+    const [filteredBooks, setFilteredBooks] = useState([]);
 
+    //Variable que utilizaremos para la paginación
+    const booksPerPage = 20;
 
     useEffect(() => {
         fetch('https://raw.githubusercontent.com/benoitvallon/100-best-books/master/books.json')
+            // Una vez que haga fetch, se obtiene una respuesta que es convertida en un objeto json que podemos manipular
             .then(response => response.json())
-            .then(books => setBooks(books))
+            .then(books => {
+                // La información que tenemos ya en json, se la pasamos a los hooks de estado (setBooks y setFilteredBooks)
+                setBooks(books);
+                // 
+                setFilteredBooks(books);
+            });
     }, []);
 
+    // Cálculo de los libros actuales, calcula el índice del primer y último libro de la pág actual
     const indexOfLastBook = currentPage * booksPerPage;
     const indexOfFirstBook = indexOfLastBook - booksPerPage;
-    const currentBooks = books.slice(indexOfFirstBook, indexOfLastBook);
+    const currentBooks = filteredBooks.slice(indexOfFirstBook, indexOfLastBook);
 
     // Cambiar a la página siguiente
     const nextPage = () => {
@@ -27,34 +38,58 @@ function Books() {
         setCurrentPage(currentPage - 1);
     };
 
+    // Función para manejar el cambio en el filtro
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value);
+    };
+
+    // Filtrar los libros por título, autor, año o país 
+    useEffect(() => {
+        const filtered = books.filter(
+            book =>
+                book.title.toLowerCase().includes(filter.toLowerCase()) ||
+                book.author.toLowerCase().includes(filter.toLowerCase()) ||
+                book.year.toString().includes(filter) ||
+                book.country.toLowerCase().includes(filter.toLowerCase())
+        );
+        setFilteredBooks(filtered);
+        setCurrentPage(1); // Resetear la página actual al aplicar un filtro
+    }, [books, filter]);
+
+
     return (
         <div>
-          {currentBooks.map((book, index) => (
-            <div className='card' key={index}>
-              <h1>{book.title}</h1>
-              <h2>{book.author}</h2>
-              <h2>{book.year}</h2>
-              <h2>{book.country}</h2>
+            <div className='filtro'>
+                <input className='inputFiltro' type="text" value={filter} onChange={handleFilterChange} placeholder="Filtrar por título, autor, año o país" />
             </div>
-          ))}
-          <div className='botones'>
-            <button
-              className={currentPage === 1 ? 'boton1off' : 'boton1on'}
-              onClick={previousPage}
-              disabled={currentPage === 1}
-            >
-              Anterior
-            </button>
-            <button
-              className='boton2'
-              onClick={nextPage}
-              disabled={indexOfLastBook >= books.length}
-            >
-              Siguiente
-            </button>
-          </div>
+            {currentBooks.map((book, index) => (
+                <div className='card' key={index}>
+                    <h1>{book.title}</h1>
+                    <h2>{book.author}</h2>
+                    <h2>{book.year}</h2>
+                    <h2>{book.country}</h2>
+                </div>
+            ))}
+            <div className='botones'>
+                <button
+                    //Produce el cambio de apariencia del botón 'anterior' en la primera página, y después la normaliza en las siguientes
+                    className={currentPage === 1 ? 'boton1off' : 'boton1on'}
+                    onClick={previousPage}
+                    disabled={currentPage === 1}
+                >
+                    Anterior
+                </button>
+                <button
+                    onClick={nextPage}
+                    //Produce el cambio de apariencia del botón siguiente cuando llega a la última página
+                    className={`boton2 ${indexOfLastBook >= filteredBooks.length || currentBooks.length === 0 ? 'boton2off' : 'boton2on'}`}
+                    disabled={indexOfLastBook >= filteredBooks.length || currentBooks.length === 0}
+                >
+                    Siguiente
+                </button>
+            </div>
         </div>
-      );
-    };
-    
-    export default Books;
+    );
+};
+
+export default Books;
